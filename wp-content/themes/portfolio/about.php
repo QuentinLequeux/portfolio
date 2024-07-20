1a894d1c56e351524da42f5660772a2dc528302f
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2024-07-20 20:41:13 +0200

    work on wordpress

--- wp-content/themes/portfolio/about.php
+++ wp-content/themes/portfolio/about.php
@@ -5,10 +5,6 @@ Template Name: A propos
 ?>
 <?= get_header(); ?>
     <main class="about">
-        <section>
-            <h2 aria-level="2" role="heading">
-                <?= get_the_title(24); ?>
-            </h2>
-        </section>
+        <?= get_the_content(false, true, 24); ?>
     </main>
 <?= get_footer(); ?>
