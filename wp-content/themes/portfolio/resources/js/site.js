e373eb9d73ec4df85008d8ba1a5c9ae0cfedcafa
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2024-08-04 21:50:50 +0200

    work on wordpress

--- wp-content/themes/portfolio/resources/js/site.js
+++ wp-content/themes/portfolio/resources/js/site.js
@@ -5,3 +5,15 @@ actives.forEach((active) => {
         active.classList.add("active");
     }
 });
+
+const left = document.body.querySelector(".left");
+const right = document.body.querySelector(".right");
+const slider = document.body.querySelector(".timeline");
+
+left.addEventListener('click', () => {
+    slider.scrollBy(-500, 0);
+});
+
+right.addEventListener('click', () => {
+    slider.scrollBy(500, 0);
+});
